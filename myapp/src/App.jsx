512d01f8910acb4d1a64f4264c82b63de97dfb66
anugrahsingh07 <anugrahsingh7777@gmail.com>
2024-11-12 19:45:22 +0530

    first commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+// import gsap from 'gsap';
+// import { useGSAP } from '@gsap/react';
+// gsap.registerPlugin(useGSAP);
+
+
+
+function App() {
+  // useGSAP(() => {
+  //   // Your GSAP animations will go here
+  //   gsap.to(".box", {
+  //     x: 1200,
+  //     duration: 1.5,
+  //     delay: 1,
+  //     ease: "power2.inOut",
+  //   });
+   
+  // });
+
+  return (
+    <>
+    <main className='w-screen h-screen bg-black text-white'>
+      
+      <h1 className='text-white'>hello</h1>
+    </main>
+    </>
+  );
+}
+
+export default App
---
+++ myapp/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(useGSAP);
+function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
